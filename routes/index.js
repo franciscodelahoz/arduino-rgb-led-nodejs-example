b1802cbd4cce1f0d5e49bae94ae6493cbbd80743
Francisco De La Hoz <franciscodelahoz18@outlook.com>
2021-03-06 07:30:47 -0500

    Fixes for recent libraries updates with breaking changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
+
 const router  = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('index');
+	res.set('Content-Type', 'text/html');
+	res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
