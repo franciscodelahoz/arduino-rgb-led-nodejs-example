6aca23b2b73a2229b2b31a53901ce31b43bd80cd
Francisco De La Hoz <franciscodelahoz18@outlook.com>
2021-03-06 07:35:54 -0500

    Format

--- bin/authentication/auth.js
+++ bin/authentication/auth.js
@@ -3,9 +3,11 @@ const admins = require('./users');
 
 module.exports = function(request, response, next) {
 	const user = auth(request);
+
 	if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
 		response.set('WWW-Authenticate', 'Basic realm="Secure Area"');
 		return response.status(401).send('Access denied');
 	}
+
 	return next();
 };
